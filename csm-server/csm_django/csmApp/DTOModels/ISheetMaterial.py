6985e21d6e31db56369ae54afae9b9f91e8dcd9b
Grigory Vasilev <grishavasilev62@gmail.com>
2023-05-03 22:08:33 +0300

    add sheet material interrface

---
+++ csm-server/csm_django/csmApp/DTOModels/ISheetMaterial.py
@@ -0,0 +1,26 @@
+from .IDTOModel import IDTOModel
+from ..Controllers.ApiErrors import *
+
+# Model of seet material.
+class ISheetMaterial(IDTOModel):
+    lenght: int
+    width: str
+    fitness: int
+    material_id: str
+
+    @staticmethod
+    def validate(sheet):
+        if (not hasattr(sheet, "name") or not sheet.name):
+            raise DataValidationError("Missed required parameter: name")
+
+        if (not hasattr(sheet, "length") or not sheet.length):
+            raise DataValidationError("Missed required parameter: length")
+    
+        if (not hasattr(sheet, "width") or not sheet.width):
+            raise DataValidationError("Missed required parameter: width")
+
+        if (not hasattr(sheet, "fitness") or not sheet.fitness):
+            raise DataValidationError("Missed required parameter: fitness")
+        
+        if (not hasattr(sheet, "material_id") or not sheet.material_id):
+            raise DataValidationError("Missed required parameter: material_id")
