f51419a07071cfb1ac82cf0389f8df89b39d3998
Grigory Vasilev <grishavasilev62@gmail.com>
2023-05-13 09:30:14 +0300

    add component

---
+++ client/csm-client/src/app/LeftMenu/lefmenu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'leftmenu',
+  template: '',
+  styles: [``] 
+})
+export class LeftMenuComponent {
+  title = 'Cutting Sheet Material';
+}
