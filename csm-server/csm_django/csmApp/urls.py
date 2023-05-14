770a8c9d4d3c21c91719bf2df9d074f2dee31a0e
Grigory Vasilev <grishavasilev62@gmail.com>
2023-05-14 21:20:17 +0300

    creating end point

--- csm-server/csm_django/csmApp/urls.py
+++ csm-server/csm_django/csmApp/urls.py
@@ -3,8 +3,10 @@ from . import views
 from django.urls import path, re_path, include
 from .Controllers.DetailController import DetailController
 from .Controllers.SheetMaterialController import SheetMaterialController
+from .Controllers.OrdersControlle import OrderController
 
 urlpatterns = [
     re_path(r'details/$', DetailController.handle_request),
     re_path(r'materials/$', SheetMaterialController.handle_request),
+    re_path(r'orders/$', OrderController.handle_request)
 ]
