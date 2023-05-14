5edcda289d01d44b04346f964222e47180227e2d
Grigory Vasilev <grishavasilev62@gmail.com>
2023-05-14 19:02:49 +0300

    Добавление нескольких компоненттов

--- client/csm-client/src/app/app.module.ts
+++ client/csm-client/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LeftMenuComponent } from './LeftMenu/leftmenu.component'
+import { MainMenuComponent} from './Main/main.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    LeftMenuComponent
+    LeftMenuComponent,
+    MainMenuComponent
   ],
   imports: [
     BrowserModule,
