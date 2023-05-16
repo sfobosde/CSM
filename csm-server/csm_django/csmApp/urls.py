bdef289a77c6bd3d9d7e2e363c96288c2a8c6e39
Grigory Vasilev <grishavasilev62@gmail.com>
2023-05-16 23:20:12 +0300

    remake all detail and template methods

--- csm-server/csm_django/csmApp/urls.py
+++ csm-server/csm_django/csmApp/urls.py
@@ -6,8 +6,17 @@ from .Controllers.SheetMaterialController import SheetMaterialController
 from .Controllers.OrderController import OrderController
 
 urlpatterns = [
-    re_path(r'details/$', DetailController.handle_request),
     re_path(r'materials/$', SheetMaterialController.handle_request),
+
+    # Create new detail.
+    path("detail/add", DetailController.create_detail),
+    # Create new detail.
+    path("detail/all", DetailController.get_all_details),
+
+    # Create new template.
+    path("template/add", DetailController.create_template),
+    # Get all templates.
+    path("template/all", DetailController.get_all_templates),
 
     # Create order.
     path("order/add", OrderController.create_order),
